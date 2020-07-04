b25614896c0c8ca3ca5a28fd950e2e151efbfd61
DANIEL MAIA DE AZEREDO <daniel.maia.jr12@gmail.com>
2020-07-04 20:37:57 -0300

    changes in db config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-module.exports = {
-	dialect: 'postgres',
-	host: 'localhost',
-	username: 'postgres',
-	password: '123456',
-	database: 'test_store',
-	define: {
-		timestamps: true,
-		underscored: true
-	}
-};
+let config;
+
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+	// the application is executed on Heroku ... use the postgres database
+	config = {
+		dialect: 'postgres',
+		protocol: 'postgres',
+		port: match[4],
+		host: match[3],
+		define: {
+			timestamps: true,
+			underscored: true
+		}
+	};
+} else {
+	config = {
+		dialect: 'postgres',
+		host: 'localhost',
+		username: 'postgres',
+		password: '123456',
+		database: 'test_store',
+		define: {
+			timestamps: true,
+			underscored: true
+		}
+	};
+}
+
+module.exports = config;
